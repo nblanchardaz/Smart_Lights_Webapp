881c3d4702fdd97a5a9df6b50210ed0552e889cf
Nicolas Blanchard <nblanchardaz@gmail.com>
2024-09-15 14:06:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function connectToDevice() {
     console.log('Initializing Bluetooth...');
     navigator.bluetooth.requestDevice({
         filters: [{name: deviceName}],
-        optionalServices: [bleService]
+        // optionalServices: [bleService]
         // acceptAllDevices: true,
         // optionalServices: [bleService]
     })
